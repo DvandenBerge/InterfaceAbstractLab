39f0d918b4ba7e830ee3898589cc4be9bdc18991
Dan <Dan@Centurion.attlocal.net>
2015-09-11 11:38:02 -0500

    Added tests to driver class

--- src/lab1/Driver.java
+++ src/lab1/Driver.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab1;
 
 /**
- *
  * @author Dan
  */
 public class Driver {
-    
+    public static void main(String[] args) {
+        ITCourse[] schedule = {
+            new IntroToProgrammingCourse("Intro to Programming","300-101"),
+            new IntroJavaCourse("Intro to Java","300-105"),
+            new AdvancedJavaCourse("Advanced Java Designs","300-201")
+        };
+        
+        for(ITCourse x:schedule){
+            System.out.println(x.getCourseName());
+            System.out.println(x.getCourseNumber());
+        }
+    }
 }
