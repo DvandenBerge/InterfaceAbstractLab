7eeb2f229681f466ef0fb6044b0c9fe5d7f34dd4
Dan <Dan@Centurion.attlocal.net>
2015-09-10 21:02:14 -0500

    Fixed set preqeq method in introjava

--- src/lab2/IntroJavaCourse.java
+++ src/lab2/IntroJavaCourse.java
@@ -69,6 +69,11 @@ public class IntroJavaCourse implements ICourse{
     }
 
     public void setPrerequisites(String prerequisites) {
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
+            System.exit(0);
+        }
         this.prerequisites = prerequisites;
     }
 
