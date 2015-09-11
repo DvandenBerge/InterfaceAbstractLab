9b0dc5415119f29e2f381f437448191fd6c9f353
Dan <Dan@Centurion.attlocal.net>
2015-09-10 20:47:50 -0500

    Added abstract methods for course name, number and credits to ICourse interface

--- src/lab2/ICourse.java
+++ src/lab2/ICourse.java
@@ -4,5 +4,12 @@ package lab2;
  * @author Dan
  */
 public interface ICourse {
+    public abstract String getCourseName();
+    public abstract String getCourseNumber();
+    public abstract double getCredits();
+    
+    public abstract void setCourseName(String courseName);
+    public abstract void setCourseNumber(String courseNumber);
+    public abstract void setCourseCredits(double credits);
     
 }
