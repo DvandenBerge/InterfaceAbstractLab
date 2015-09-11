d918d94cd9fd91c525ed913b332306c76f051c52
Dan <Dan@Centurion.attlocal.net>
2015-09-10 21:14:00 -0500

    Added more tests to driver

--- src/lab2/Driver.java
+++ src/lab2/Driver.java
@@ -11,9 +11,14 @@ public class Driver {
             new AdvancedJavaCourse("Advanced Java Designs", "300-201")
         };
         
+        courses[0].setCredits(2.0);
+        courses[1].setCredits(3.0);
+        courses[2].setCredits(4.0); 
+        
         for(ICourse c: courses){
             System.out.println(c.getCourseName());
             System.out.println(c.getCourseNumber());
+            System.out.println(c.getCredits());
         }
     }
  
