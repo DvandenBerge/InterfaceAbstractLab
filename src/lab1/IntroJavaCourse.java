23a894f1ca65802382c3c0dfc4ecdac30125caf4
Dan <Dan@Centurion.attlocal.net>
2015-09-11 11:28:53 -0500

    Eddited IntroJavaCourse to have correct encapsulation and removed extraneous methods. Changed setPrereequisests method to error check

--- src/lab1/IntroJavaCourse.java
+++ src/lab1/IntroJavaCourse.java
@@ -1,5 +1,7 @@
 package lab1;
 
+import javax.swing.JOptionPane;
+
 /**
  * Describe responsibilities here.
  *
@@ -7,44 +9,27 @@ package lab1;
  * @version     1.00
  */
 public class IntroJavaCourse extends ITCourse{
-    String courseName;
+    private String courseName;
     private String courseNumber;
     private double credits;
     private String prerequisites;
 
     public IntroJavaCourse(String courseName, String courseNumber) {
-        this.courseName = courseName;
-        this.courseNumber = courseNumber;
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
     }
 
-    public String getCourseNumber() {
-        return courseNumber;
+    public String getPrerequisets(){
+        return this.prerequisites;
     }
-
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
-
-    public double getCredits() {
-        return credits;
-    }
-
-
-    public String getPrerequisites() {
-        return prerequisites;
-    }
-
+    
     public void setPrerequisites(String prerequisites) {
-        this.prerequisites = prerequisites;
-    }
-
-        public void setCredits(double credits) {
-        if(credits < 0 || credits > 5.0) {
-            System.out.println(
-                    "Error: credits must be in the range 0.5 to 4.0");
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
             System.exit(0);
         }
-        this.setCredits(credits);
+        this.prerequisites = prerequisites;
     }
 
 }
